f40716d3e8337f49741476ee4384edab438c41eb
dsw <dishuwang@126.com>
2021-07-24 20:07:46 +0800

    first commit

---
+++ src/Greet.php
@@ -0,0 +1,18 @@
+<?php
+/***************************************************************************
+ * @Description:
+ * 
+ * @FileName:         Greet.php
+ * @Author :          shuwang@staff.sina.com.cn
+ * @CreatedTime:      2021年07月24日 星期六 20时02分53秒
+ ************************************************************************/
+namespace Greet;
+
+class Say
+{
+    public static function hello()
+    {
+        echo "hello world";
+    }
+}
+
